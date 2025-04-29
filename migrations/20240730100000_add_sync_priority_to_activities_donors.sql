365583d22b5f219347095b3da29626952c5b8b25
sagarsth <sagarsth4714@gmail.com>
2025-04-29 14:48:36 +1000

    Fix migration checksum, update prepare data, and ignore dev.db

--- migrations/20240730100000_add_sync_priority_to_activities_donors.sql
+++ migrations/20240730100000_add_sync_priority_to_activities_donors.sql
@@ -1,5 +1,4 @@
 -- Add sync_priority column to activities
 ALTER TABLE activities ADD COLUMN sync_priority INTEGER NOT NULL DEFAULT 5; -- Default to Normal (5)
- 
 -- Add sync_priority column to donors
 ALTER TABLE donors ADD COLUMN sync_priority INTEGER NOT NULL DEFAULT 5; -- Default to Normal (5) 
